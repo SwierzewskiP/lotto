2d404abe4bb223886f207435c8622b6b7dc1f878
Piotr Świerzewski <swierzewski.piotr@gmail.com>
2024-01-14 20:35:47 +0100

    change in requirements - now we use external service as numbers generator

--- src/main/java/pl/swierzewski/domain/numbergenerator/dto/SixRandomNumbersDto.java
+++ src/main/java/pl/swierzewski/domain/numbergenerator/dto/SixRandomNumbersDto.java
@@ -2,8 +2,8 @@ package pl.swierzewski.domain.numbergenerator.dto;
 
 import lombok.Builder;
 
+import java.util.Set;
+
 @Builder
-public record OneRandomNumberResponseDto(
-        int number
-) {
+public record SixRandomNumbersDto(Set<Integer> numbers) {
 }
